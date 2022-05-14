98cdc0604998a25e3c7fe57c368e92bda6ee4641
“Elsa-zlt” <2903250242@qq.com>
2022-05-14 12:19:50 +0800

    commit

--- item-restful-web/src/main/java/com/web/items/controller/ItemsController.java
+++ item-restful-web/src/main/java/com/web/items/controller/ItemsController.java
@@ -1,11 +1,10 @@
 package com.web.items.controller;
 
 import com.web.items.pojo.Items;
+import com.web.items.pojo.RespBean;
 import com.web.items.service.ItemsService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,7 +16,6 @@ public class ItemsController {
     @Autowired
     private ItemsService itemsService;
 
-    //
     @GetMapping("findAll")
     public List<Items> findAll() {
         return itemsService.findAll();
@@ -27,5 +25,49 @@ public class ItemsController {
     public Items findOne(@PathVariable int id) {
         return itemsService.findOne(id);
     }
+
+    // @RequestBody 把 json 格式转换成 Java 对象
+    @PostMapping("/addItems")
+    public RespBean addItems(@RequestBody Items items) {
+        System.out.println(items.getName());
+        System.out.println(items.getDetail());
+        try {
+            // id会回填到items对象中
+            itemsService.addItems(items);
+            return RespBean.ok("添加成功",items);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return RespBean.err("添加失败");
+        }
+    }
+
+    @PutMapping("/updateItems")
+    public RespBean updateItems(@RequestBody Items items) {
+        System.out.println(items.getName());
+        System.out.println(items.getDetail());
+        try {
+            // id会回填到items对象中
+            itemsService.updateItems(items);
+            return RespBean.ok("修改成功",items);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return RespBean.err("修改失败");
+        }
+    }
+    @DeleteMapping("/deleteItems/{id}")
+    public RespBean deleteItems(@PathVariable int id) {
+        try {
+            // id会回填到items对象中
+            itemsService.deleteItems(id);
+            return RespBean.ok("删除成功");
+        } catch (Exception e) {
+            e.printStackTrace();
+            return RespBean.err("删除失败");
+        }
+    }
+
+
+
+
 
 }
